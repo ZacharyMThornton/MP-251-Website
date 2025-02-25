d01665f523505de177dccfc29418233b162a79b4
Zachary Thornton <thorntonzachary02@gmail.com>
2025-02-25 12:21:28 -0500

    Delete projects directory

--- projects/projects.js
+++
@@ -1,9 +0,0 @@
-const links = ["../home/index.html", "../lightningTalks/lightningTalks.html", "../experiences/experiences.html", "https://github.com/ZacharyMThornton"];
-
-const linkButtons = document.getElementsByClassName("link");
-
-for (let i=0; i <= linkButtons.length - 1; i++){
-    linkButtons[i].addEventListener("click", () => {
-        window.location.replace(links[i]);
-    })
-}
